0d9888ef0f1760e18bbe1889a3d373fa926ec753
Ramazan Köylü <ramazankoylu4040@gmail.com>
2023-03-09 01:44:07 +0300

    odev yapildi

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Form from './NewTodoForm'
+
+function Header() 
+  return 
+   < className="Header">
+		<h1>todos</h1>
+		<Form />
+    <Header/>
+  
+
+
+export default Header
